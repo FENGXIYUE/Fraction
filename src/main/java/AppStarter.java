1503483024e33bde2987665d39b5730fc19970bf
LIULONGTAO <nwpullt@163.com>
2021-08-02 18:05:31 +0800

    [20210802]-feat:主流程新增异常抛出情况

--- src/main/java/AppStarter.java
+++ src/main/java/AppStarter.java
@@ -1,3 +1,6 @@
+import exception.ArgumentNotValidException;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Scanner;
 
 /**
@@ -6,6 +9,7 @@ import java.util.Scanner;
  * @date: 2021/8/2 3:33 下午
  * @modified By
  */
+@Slf4j
 public class AppStarter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -15,24 +19,29 @@ public class AppStarter {
                 + "\r\n" + "2 分数间运算"
                 + "\r\n" + "3 退出此程序");
         while (true) {
-            String inputLine = scanner.nextLine();
-            if ("3".equals(inputLine)) {
-                break;
-            } else if ("1".equals(inputLine)) {
-                System.out.println("待转化分数");
-                String fraction = scanner.nextLine();
-                System.out.println("结果为：");
-            } else if ("2".equals(inputLine)) {
-                System.out.println("运算表达式");
-                String expression = scanner.nextLine();
-                System.out.println("结果为：");
-            } else {
-                System.out.println("编码未识别："
+            try {
+                String inputLine = scanner.nextLine();
+                if ("3".equals(inputLine)) {
+                    break;
+                } else if ("1".equals(inputLine)) {
+                    System.out.println("待转化分数");
+                    String fraction = scanner.nextLine();
+                    System.out.println("结果为：");
+                } else if ("2".equals(inputLine)) {
+                    System.out.println("运算表达式");
+                    String expression = scanner.nextLine();
+                    System.out.println("结果为：");
+                } else {
+                    throw new ArgumentNotValidException("输入编码异常");
+                }
+            }catch (ArgumentNotValidException e){
+                log.error(e.getMessage()
                         + "\r\n" + "请输入正确编码"
                         + "\r\n" + "1 分数转小数"
                         + "\r\n" + "2 分数间运算"
                         + "\r\n" + "3 退出此程序");
             }
+
 
         }
     }
