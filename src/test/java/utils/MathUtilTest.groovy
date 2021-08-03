8540abb00caa65c2091f7e3c73201ecf4eaedb8e
LIULONGTAO <nwpullt@163.com>
2021-08-03 09:40:52 +0800

    [20210803]-test:新增工具类MathUtilTest测试类

---
+++ src/test/java/utils/MathUtilTest.groovy
@@ -0,0 +1,24 @@
+package utils
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * @author: LLT
+ * @description:
+ * @date: 2021/8/3 9:32 上午
+ * @modified By
+ */
+class MathUtilTest extends Specification {
+    @Unroll
+    def "GetCommonFactor"() {
+        expect:
+        result == MathUtil.getCommonFactor(a, b)
+        where:
+        result | a | b
+        2      | 4 | 2
+        2      | 2 | 4
+        4      | 4 | 4
+
+    }
+}
